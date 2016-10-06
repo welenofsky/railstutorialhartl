cc168ffeaf9be4cdec101a3d355a68ecd1b56ec1
Justin Welenofsky <welenofsky@gmail.com>
2016-10-05 21:35:32 -0700

    micropost scaffolding

--- toy_app/db/schema.rb
+++ toy_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006033247) do
+ActiveRecord::Schema.define(version: 20161006043226) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
